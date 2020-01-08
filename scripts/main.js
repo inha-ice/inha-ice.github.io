1ae7dfb2f10f1c180df8b11deb8a41e1db450a68
Seungjae Park <astro.psj@gmail.com>
2020-01-09 01:21:24 +0900

    Initial layout

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+const nav = document.querySelector('.nav');
+const container = document.querySelector('.container');
+
+const scrollTo = (y) => {
+  setTimeout(() => {
+    window.scrollTo({
+      top: y,
+      left: 0,
+      behavior: 'smooth',
+    });
+  }, 1);
+};
+
+// nav color
+window.addEventListener('scroll', () => {
+  if (window.scrollY === 0) { // top
+    nav.classList.remove('active');
+  } else {
+    nav.classList.add('active');
+  }
+});
+
+// fullscreen scroll
+window.addEventListener('wheel', (event) => {
+  const threshold = container.offsetTop - 100;
+  if (window.scrollY < threshold) {
+    if (event.deltaY > 0) { // down
+      scrollTo(threshold);
+    } else { // up
+      scrollTo(0);
+    }
+  }
+});
